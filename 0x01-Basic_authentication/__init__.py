c7c7dac09c64c2eb405e701a46647dfe81d37556
Tommyokoyo <okoyotommy@gmail.com>
2023-08-09 01:38:12 +0300

    Error handler: Forbidden

---
+++ 0x01-Basic_authentication/__init__.py
@@ -0,0 +1,3 @@
+"""
+    Basic Auth module
+"""
--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,18 +14,6 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """
-        Request unauthorized handler
-        Args:
-            error:
-
-        Returns: JSON{"error:Not found"}
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """
@@ -36,6 +24,30 @@ def unauthorized_error(error) -> str:
         Returns: JSON{"error:Unauthorized"}
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """
+        Request forbidden handler
+        Args:
+            error:
+
+        Returns: JSON{"error:Forbidden"}
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """
+        Request unauthorized handler
+        Args:
+            error:
+
+        Returns: JSON{"error:Not found"}
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
