2e64c274863220695fb37f1e3876fd88258228c6
Tommyokoyo <okoyotommy@gmail.com>
2023-08-09 01:20:47 +0300

    Error handler: Unauthroized

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,9 +16,26 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+        Request unauthorized handler
+        Args:
+            error:
+
+        Returns: JSON{"error:Not found"}
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """
+        Request unauthorized handler
+        Args:
+            error:
+
+        Returns: JSON{"error:Unauthorized"}
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
