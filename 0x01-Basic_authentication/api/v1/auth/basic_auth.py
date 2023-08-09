64d6ff0e66c5d7a317d411d2afe3dd4f1e024e52
Tommyokoyo <okoyotommy@gmail.com>
2023-08-10 00:22:52 +0300

    Basic - Base64 decode (styling)

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,6 +11,7 @@ class BasicAuth(Auth):
     """
         Basic auth class
     """
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """
@@ -33,7 +34,7 @@ class BasicAuth(Auth):
             return: decoded value of Base64
         """
         if (base64_authorization_header is None or type
-            (base64_authorization_header) is not str):
+                (base64_authorization_header) is not str):
             return None
         else:
             try:
