ea8b2f8e0d274d8c3387ee3e37cd0bd85cc7cb1b
tommyokoyo <okoyotommy@gmail.com>
2023-08-13 08:52:30 -0400

    Code Edit

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,8 +14,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE', 'basic_auth')
 if AUTH_TYPE == 'auth':
-     from api.v1.auth.auth import Auth
-     auth = Auth()
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
