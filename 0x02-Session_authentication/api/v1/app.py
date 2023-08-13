70c2bf01eccafcf1d7956968ede9641d610c2b7e
tommyokoyo <okoyotommy@gmail.com>
2023-08-13 17:30:14 -0400

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -66,12 +66,13 @@ def before_request() -> str:
         Executed before a request is handled
     """
     ex_path = ['/api/v1/status/', '/api/v1/unauthorized/',
-               '/api/v1/forbidden/']
+               '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
     elif not auth.require_auth(request.path, ex_path):
         return
-    elif auth.authorization_header(request) is None:
+    elif (auth.authorization_header(request) is None and
+          auth.session_cookie(request) is None):
         abort(401)
     elif auth.current_user(request) is None:
         abort(403)
