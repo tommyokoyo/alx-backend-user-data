10526c48ef33bd95d27472704b2783a660552598
tommyokoyo <okoyotommy@gmail.com>
2023-08-29 17:13:56 +0300

    Logout - documentation

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,7 +8,8 @@ from flask import jsonify, request
 from models.user import User
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
 def login():
     """
         POST /auth_session/login
@@ -25,17 +26,20 @@ def login():
         users_found = User.search({"email": user_email})
 
         if not users_found:
-            return jsonify({"error": "no user found for this email"}), 404
+            return jsonify({"error":
+                                "no user found for this email"}), 404
         else:
             for user in users_found:
                 if user.is_valid_password(user_pass):
                     from api.v1.app import auth
                     session_id = auth.create_session(user.id)
                     response = jsonify(user.to_json())
-                    response.set_cookie(getenv('SESSION_NAME'), session_id)
+                    response.set_cookie(getenv('SESSION_NAME'),
+                                        session_id)
                     return response
                 else:
-                    return jsonify({"error": "wrong password"}), 401
+                    return jsonify({"error":
+                                        "wrong password"}), 401
 
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
